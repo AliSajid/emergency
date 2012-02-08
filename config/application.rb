0af86ce5b223c97a2b7203218c7be1207ae20478
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2012-02-09 00:37:23 +0500

    Generated the barista configuration, which i don't believe was necessary. Also added a print stylesheet and a link to "Print a page". Let's hope it works.

--- config/application.rb
+++ config/application.rb
@@ -27,20 +27,20 @@ module Emergency
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+	config.time_zone = 'Karachi'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+	config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+	config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+	config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
